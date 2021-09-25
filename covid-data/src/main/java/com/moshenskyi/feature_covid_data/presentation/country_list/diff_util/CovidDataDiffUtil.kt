b380d3e517545e338a84125b66e39e1386eb9074
moshenskyi_n <moshenskyi.nazariy@gmail.com>
2021-09-25 10:02:22 +0300

    feat(covid-list): Reformat code
    Reformatted code

--- covid-data/src/main/java/com/moshenskyi/feature_covid_data/presentation/country_list/diff_util/CovidDataDiffUtil.kt
+++ covid-data/src/main/java/com/moshenskyi/feature_covid_data/presentation/country_list/diff_util/CovidDataDiffUtil.kt
@@ -4,15 +4,15 @@ import androidx.recyclerview.widget.DiffUtil
 import com.moshenskyi.feature_covid_data.domain.model.CovidInfoEntity
 
 class CovidDataDiffUtil : DiffUtil.ItemCallback<CovidInfoEntity>() {
-    override fun areItemsTheSame(oldItem: CovidInfoEntity, newItem: CovidInfoEntity): Boolean {
-        return oldItem.country == newItem.country
-    }
+	override fun areItemsTheSame(oldItem: CovidInfoEntity, newItem: CovidInfoEntity): Boolean {
+		return oldItem.country == newItem.country
+	}
 
-    override fun areContentsTheSame(oldItem: CovidInfoEntity, newItem: CovidInfoEntity): Boolean {
-        return oldItem == newItem
-    }
+	override fun areContentsTheSame(oldItem: CovidInfoEntity, newItem: CovidInfoEntity): Boolean {
+		return oldItem == newItem
+	}
 
-    override fun getChangePayload(oldItem: CovidInfoEntity, newItem: CovidInfoEntity): Any {
-        return oldItem.expanded != newItem.expanded
-    }
+	override fun getChangePayload(oldItem: CovidInfoEntity, newItem: CovidInfoEntity): Any {
+		return oldItem.expanded != newItem.expanded
+	}
 }

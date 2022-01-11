56a1cd54b5d2ec6434ea33ca2d9537cf74d87804
moshenskyi_n <moshenskyi.nazariy@gmail.com>
2022-01-11 05:48:27 +0200

    Added imports for models

--- covid-data/src/main/java/com/moshenskyi/feature_covid_data/domain_entity/CovidEntityMapper.kt
+++ covid-data/src/main/java/com/moshenskyi/feature_covid_data/domain_entity/CovidEntityMapper.kt
@@ -1,8 +1,11 @@
 package com.moshenskyi.feature_covid_data.domain_entity
 
+import com.moshenskyi.feature_covid_data.model.general.CountryRecordsItem
+import com.moshenskyi.feature_covid_data.model.vaccine.VaccineCapacityInfoItem
+
 fun map(
-	countryInfo: List<com.moshenskyi.feature_covid_data.model.general.CountryRecordsItem>,
-	vaccineCapacityInfo: List<com.moshenskyi.feature_covid_data.model.vaccine.VaccineCapacityInfoItem>
+	countryInfo: List<CountryRecordsItem>,
+	vaccineCapacityInfo: List<VaccineCapacityInfoItem>,
 ): List<CovidInfoEntity> {
 	val infoList = mutableListOf<CovidInfoEntity>()
 
